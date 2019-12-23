040a8740248af2e4cdf9f578d493337d00a4aac8
Douglas Anderson <hockeybuggy@gmail.com>
2019-12-23 09:42:30 -0500

    Start to look into decoding.
    I was following along with the book I have, but I skipped a whole
    chapter and when straight to Json decoding.

--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,25 @@ module Main exposing (Msg(..), main, update, view)
 import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
+import Json.Decode exposing (bool, decodeString, int, string)
+
+
+type alias Id =
+    Int
+
+
+type alias Photo =
+    { id : Id
+    , url : String
+    , caption : String
+    , liked : Bool
+    , comments : List String
+    , newComment : String
+    }
+
+
+type alias Model =
+    Photo
 
 
 initalModel : { count : Int }
@@ -11,7 +30,11 @@ initalModel =
 
 
 main =
-    Browser.sandbox { init = initalModel, update = update, view = view }
+    Browser.sandbox
+        { init = initalModel
+        , update = update
+        , view = view
+        }
 
 
 type Msg
